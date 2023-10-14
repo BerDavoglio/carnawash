65435637fa1065de99a4a539977ae66f0be213c0
Ber Davoglio <berdavoglio@gmail.com>
2023-10-14 16:23:29 -0300

    Create LoginPage for AdminPanel

--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -1,66 +1,84 @@
 <!-- eslint-disable vuejs-accessibility/click-events-have-key-events -->
 <template>
   <div class="login-view">
-    <div className="mx-auto my-36">
-      <div className="max-w-[32rem] bg-blue-200 m-auto rounded-xl p-4 shadow-2xl">
-        <div className="text-bold text-[3rem] m-4">
-          {{ isLogin ? 'Login' : 'Cadastrar' }}
+    <div className="w-full h-screen grid grid-cols-2 bg-[#107EC4]">
+      <div style="clip-path: ellipse(100% 47% at 0% 50%);"
+           className="w-full h-[95%] m-auto bg-white">
+        <div className="flex h-[85vh]">
+          <img className="m-auto w-[60%]"
+               src="../../public/assets/img/brand-logo.png"
+               alt="" />
         </div>
-        <div v-if="isLogin">
+      </div>
+      <div className="m-auto w-auto"
+           v-show="false">
+        <div className="text-white mb-9 text-[22px]">
+          Sign In
+        </div>
+        <div className="w-full">
           <input v-model="email"
                  placeholder="E-mail"
-                 className="w-[90%] h-10 rounded-lg p-2 mb-2">
+                 className="h-[16px] w-full rounded-lg  px-4 py-6 mb-5">
           <input v-model="password"
-                 placeholder="Senha"
-                 className="w-[90%] h-10 rounded-lg p-2 mb-2">
+                 :type="isHiddenOne ? 'password' : 'text'"
+                 aria-label="Password"
+                 placeholder="Password"
+                 className="h-[16px] w-full rounded-lg  px-4 py-6 mb-5">
+          <div className="text-[16px] text-white underline mb-9 cursor-pointer">
+            I forgot the password
+          </div>
         </div>
-        <div v-else>
-          <input v-model="user.name"
-                 placeholder="Nome"
-                 className="w-[90%] h-10 rounded-lg p-2 mb-2">
-          <input v-model="user.email"
-                 placeholder="E-mail"
-                 className="w-[90%] h-10 rounded-lg p-2 mb-2">
-          <input v-model="user.address"
-                 placeholder="Endereço"
-                 className="w-[90%] h-10 rounded-lg p-2 mb-2">
-          <input v-model="user.cellphone"
-                 placeholder="Telefone"
-                 className="w-[90%] h-10 rounded-lg p-2 mb-2">
-          <div className="w-[90%] m-auto">
-            <VueDatePicker v-model="user.birth"
-                           format="dd/MM/yyyy"
-                           :enable-time-picker="false"
-                           input-class-name="h-10 rounded-lg" />
-          </div>
-          <input v-model="user.cpf"
-                 placeholder="CPF"
-                 className="w-[90%] h-10 rounded-lg p-2 my-2">
-          <input v-model="user.password"
-                 placeholder="Senha"
-                 className="w-[90%] h-10 rounded-lg p-2 mb-2">
+        <div className="w-full p-[12.5px] bg-[#EDBD3A]
+        text-white rounded-[8px] cursor-pointer">
+          Login
         </div>
-        <div className="grid grid-cols-2 mx-10 my-5">
-          <div className="mx-auto w-24 p-3
-            rounded-xl text-white
-            cursor-pointer bg-green-500 hover:bg-green-700"
-               @click="isLogin ? login() : signin()">
-            {{ isLogin ? 'Entrar' : 'Cadastrar' }}
+      </div>
+      <div className="m-auto w-auto max-w-[443px]"
+           v-show="false">
+        <div className="text-white mb-10 text-[22px]">
+          Retrieve your account
+        </div>
+        <div className="w-full mb-10">
+          <input v-model="email"
+                 placeholder="E-mail or phone number"
+                 className="h-[16px] w-full rounded-lg px-4 py-6 mb-5">
+          <div className="text-white text-[16px] w-[90%] m-auto">
+            We're gonna sent an e-mail to your inbox with a link to reset your password
           </div>
-          <div v-if="isLogin"
-               className="mx-auto w-24 p-3
-            rounded-xl text-white
-            cursor-pointer bg-yellow-500 hover:bg-yellow-700"
-               @click="isLogin = !isLogin">
-            Registrar
-          </div>
-          <div v-else
-               className="mx-auto w-24 p-3
-            rounded-xl text-white
-            cursor-pointer bg-yellow-500 hover:bg-yellow-700"
-               @click="isLogin = !isLogin">
-            Login
-          </div>
+        </div>
+        <div className="w-full p-[10px] bg-[#EDBD3A]
+                text-white rounded-[8px] cursor-pointer">
+          Send me the e-mail
+        </div>
+        <div className="my-[20px] text-[16px] text-white">or</div>
+        <div className="text-[16px] text-[#EDBD3A] cursor-pointer">
+          Create an account
+        </div>
+      </div>
+      <div className="m-auto w-auto"
+           v-show="true">
+        <div className="text-white mb-10 text-[22px]">
+          Change your password
+        </div>
+        <div className="w-full mb-10">
+          <input v-model="password"
+                 :type="isHiddenOne ? 'password' : 'text'"
+                 aria-label="Password"
+                 placeholder="Password"
+                 className="h-[16px] w-full rounded-lg  px-4 py-6 mb-5">
+          <input v-model="password"
+                 :type="isHiddenTwo ? 'password' : 'text'"
+                 aria-label="Confirm password"
+                 placeholder="Confirm password"
+                 className="h-[16px] w-full rounded-lg  px-4 py-6 mb-5">
+        </div>
+        <div className="w-full p-[10px] bg-[#EDBD3A]
+                text-white rounded-[8px] cursor-pointer">
+          Confirm new password
+        </div>
+        <div className="my-[20px] text-[16px] text-white">or</div>
+        <div className="text-[16px] text-[#EDBD3A] cursor-pointer">
+          Log In to my account
         </div>
       </div>
     </div>
@@ -78,15 +96,9 @@ export default {
     return {
       email: '',
       password: '',
-      user: {
-        name: '',
-        email: '',
-        address: '',
-        cellphone: '',
-        birth: '',
-        cpf: '',
-        password: '',
-      },
+      isHiddenOne: true,
+      isHiddenTwo: true,
+      pwIcon: false,
       isLogin: true,
     };
   },
@@ -116,27 +128,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.dp__theme_light {
-  --dp-background-color: #ffffff;
-  --dp-text-color: #000000;
-  --dp-hover-color: #f3f3f3;
-  --dp-hover-text-color: #212121;
-  --dp-hover-icon-color: #959595;
-  --dp-primary-color: #81c0ff;
-  --dp-primary-text-color: #959595;
-  --dp-secondary-color: #c0c4cc;
-  --dp-border-color: #ddd;
-  --dp-menu-border-color: #ddd;
-  --dp-border-color-hover: #aaaeb7;
-  --dp-disabled-color: #f6f6f6;
-  --dp-scroll-bar-background: #f3f3f3;
-  --dp-scroll-bar-color: #959595;
-  --dp-success-color: #76d275;
-  --dp-success-color-disabled: #a3d9b1;
-  --dp-icon-color: #959595;
-  --dp-danger-color: #ff6f60;
-  --dp-highlight-color: rgba(25, 118, 210, 0.1);
-}
-</style>
