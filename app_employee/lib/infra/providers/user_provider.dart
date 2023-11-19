6c490928f8f4224d6b74f73986b2710f6f567df8
Ber Davoglio <berdavoglio@gmail.com>
2023-11-19 05:42:29 -0300

    Initial Documents for App_Employeer

---
+++ app_employee/lib/infra/providers/user_provider.dart
@@ -0,0 +1,186 @@
+// ignore_for_file: use_build_context_synchronously, non_constant_identifier_names
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+import '../../data/data.dart';
+import '../../ui/ui.dart';
+import '../infra.dart';
+
+class UserProvider with ChangeNotifier {
+  String _token = '';
+  late UserCompleteModel _perfil;
+
+  String get token => _token;
+  UserCompleteModel get perfil => _perfil;
+
+  bool get isAuth {
+    return _token != '';
+  }
+
+  Future<void> submitLogin(
+      BuildContext context, String email, String password) async {
+    try {
+      final response = await http.post(
+        Uri.parse('${Constants.BACKEND_BASE_URL}/jwt/'),
+        body: {
+          "email": email,
+          "password": password,
+        },
+      );
+
+      var v = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        _token = v['token'];
+        await loadPerfil(context);
+        Navigator.of(context).pushReplacementNamed(AppRoutes.HOME);
+        notifyListeners();
+      } else {
+        await comumDialog(
+          context,
+          'Error',
+          v['errors'],
+        );
+      }
+
+      Navigator.of(context).pushNamed(AppRoutes.HOME);
+
+      notifyListeners();
+    } catch (e) {
+      await comumDialog(
+        context,
+        'Provider Error! SubmitLogin',
+        e.toString(),
+      );
+    }
+  }
+
+  Future<void> signIn(
+    BuildContext context,
+    UserSignModel user,
+  ) async {
+    try {
+      final response = await http.post(
+        Uri.parse('${Constants.BACKEND_BASE_URL}/users/'),
+        body: {
+          "email": user.email,
+          "password": user.password,
+          "name": user.name,
+          "phone": user.phone,
+          "address": user.address,
+        },
+      );
+
+      var v = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        _token = v[1];
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('Account Created! Verify your email!'),
+            action: SnackBarAction(
+              label: 'Okay',
+              onPressed: () {},
+            ),
+          ),
+        );
+        Navigator.of(context).pushReplacementNamed(AppRoutes.LOGIN);
+      } else if (v['errors'] != '') {
+        await comumDialog(
+          context,
+          'Error',
+          v['errors'],
+        );
+      }
+
+      Navigator.of(context).pushNamed(AppRoutes.HOME);
+
+      notifyListeners();
+    } catch (e) {
+      await comumDialog(
+        context,
+        'Provider Error! SignIn',
+        e.toString(),
+      );
+    }
+  }
+
+  Future<void> loadPerfil(BuildContext context) async {
+    try {
+      final response = await http.get(
+        Uri.parse('${Constants.BACKEND_BASE_URL}/user/'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $_token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        _perfil = jsonDecode(response.body);
+        notifyListeners();
+      } else if (jsonDecode(response.body)['errors'] != '') {
+        await comumDialog(
+          context,
+          'Erro!',
+          jsonDecode(response.body)['errors'],
+        );
+      }
+    } catch (e) {
+      await comumDialog(
+        context,
+        'Provider Error!',
+        'Ocorreu um erro ao tentar obter os valores de perfil. $e',
+      );
+    }
+  }
+
+  Future<void> updatePerfil(
+      BuildContext context, UserCompleteModel newUser) async {
+    try {
+      final response = await http.put(
+        Uri.parse('${Constants.BACKEND_BASE_URL}/user/'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $_token',
+        },
+        body: jsonEncode(newUser),
+      );
+
+      var v = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        await comumDialog(
+          context,
+          'Usuário Atualizado',
+          'Usuário atualizado com Sucesso!',
+        );
+        await loadPerfil(context);
+        Navigator.of(context).pop();
+        notifyListeners();
+      } else if (v['errors'] != '') {
+        await comumDialog(
+          context,
+          'Erro!',
+          v['errors'],
+        );
+      }
+    } catch (e) {
+      await comumDialog(
+        context,
+        'Provider Error!',
+        e.toString(),
+      );
+    }
+  }
+
+  void logout() {
+    _token = '';
+    notifyListeners();
+  }
+}
--- app_employee/lib/ui/components/app.dart
+++ app_employee/lib/ui/components/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../infra/infra.dart';
 import '../ui.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,31 +10,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'CarWash',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'CarWash',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          AppRoutes.HOME: (_) => const HomePage(),
+          AppRoutes.FIRST_LOGIN_HOME: (_) => const FirstLoginHomePage(),
+          AppRoutes.LOGIN: (_) => const LoginPage(),
+          AppRoutes.SCHEDULES: (_) => const SchedulesPage(),
+          AppRoutes.SUPPLY: (_) => const SupplyPage(),
+          AppRoutes.PAYMENTS: (_) => const PaymentsPage(),
+          AppRoutes.HELP: (_) => const HelpPage(),
+          AppRoutes.HELP_CHAT: (_) => const HelpChatPage(),
+          AppRoutes.PERFIL: (_) => const PerfilPage(),
+          AppRoutes.ACCOUNT: (_) => const AccountPage(),
+          AppRoutes.WALLET: (_) => const WalletPage(),
+          AppRoutes.LANGUAGE: (_) => const LanguagePage(),
+          AppRoutes.NOTIFICATION_CONFIG: (_) => const NotificationConfigPage(),
+          AppRoutes.NOTIFICATION: (_) => const NotificationPage(),
+          AppRoutes.TERMS: (_) => const TermsConditionsPage(),
+          AppRoutes.AVAILABILITY: (_) => const AvailabilityPage(),
+          AppRoutes.WASH_REQUEST: (_) => const WashRequestPage(),
+        },
       ),
-      routes: {
-        AppRoutes.HOME: (_) => const HomePage(),
-        AppRoutes.FIRST_LOGIN_HOME: (_) => const FirstLoginHomePage(),
-        AppRoutes.LOGIN: (_) => const LoginPage(),
-        AppRoutes.SCHEDULES: (_) => const SchedulesPage(),
-        AppRoutes.SUPPLY: (_) => const SupplyPage(),
-        AppRoutes.PAYMENTS: (_) => const PaymentsPage(),
-        AppRoutes.HELP: (_) => const HelpPage(),
-        AppRoutes.HELP_CHAT: (_) => const HelpChatPage(),
-        AppRoutes.PERFIL: (_) => const PerfilPage(),
-        AppRoutes.ACCOUNT: (_) => const AccountPage(),
-        AppRoutes.WALLET: (_) => const WalletPage(),
-        AppRoutes.LANGUAGE: (_) => const LanguagePage(),
-        AppRoutes.NOTIFICATION_CONFIG: (_) => const NotificationConfigPage(),
-        AppRoutes.NOTIFICATION: (_) => const NotificationPage(),
-        AppRoutes.TERMS: (_) => const TermsConditionsPage(),
-        AppRoutes.AVAILABILITY: (_) => const AvailabilityPage(),
-        AppRoutes.WASH_REQUEST: (_) => const WashRequestPage(),
-      },
     );
   }
 }
