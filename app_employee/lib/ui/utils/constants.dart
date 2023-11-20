a3313fbfc932f2eda53ad0a943861672708a105d
Ber Davoglio <berdavoglio@gmail.com>
2023-11-20 16:07:34 -0300

    Almost finished all WasherProviderCode / 4

--- app_employee/lib/ui/utils/constants.dart
+++ app_employee/lib/ui/utils/constants.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
 class Constants {
-  static const BACKEND_BASE_URL = 'http://10.0.2.2:3096';
+  static const BACKEND_BASE_URL = 'http://191.96.251.106:3096';
   static const STRIPE_KEY = '';
 }
